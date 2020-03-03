d340d41b0f7433b7e0c82552d53196a20ab45789
Denispoper <sherbakov4599@gmail.com>
2020-03-03 09:58:56 +0300

    archiveArtifacts

--- test.py
+++ test.py
@@ -24,6 +24,6 @@ if __name__ == "__main__":
     f.setCamera(ns.objectcount)
 
 
-#>mayapy D:\Workspace\test_scpirts\test.py -ot Cube ... 
+#>mayapy D:\Workspace\test_scpirts\test.py -ot Cube -oc 3 -lt Physical -mc 2 -lc 4 -ibl true -ms 400 -th 0.001
 
 #>render -r FireRender -rd C:\Users\Denispoper\Desktop\testResults -of jpeg -im testResultImage -x 1000 -y 1000 C:\Users\Denispoper\Desktop\mayaimages\test.mb
